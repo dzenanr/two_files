52e5e2a90770dddfd4099c68895dd2f856089ede
dzenanr <dzenanr@gmail.com>
2013-04-24 19:10:32 -0400

    replace reduce by fold

--- bin/fcns.dart
+++ bin/fcns.dart
@@ -1,6 +1,6 @@
 part of two_files;
 
-num sum(List<num> list) => list.reduce(0, (prev, element) => prev + element);
+num sum(List<num> list) => list.fold(0, (prev, element) => prev + element);
 
 List reverse(List list) => list.reversed.toList();
 
